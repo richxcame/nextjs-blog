8c7aed4103527c7aaa3c31e53536ac85ac0e6edb
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-21 00:51:52 +0500

    L7: layout and css modules

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
 	return (
-		<>
+		<Layout>
 			<Head>
 				<title>First Post</title>
 				<script src='https://connect.facebook.net/en_US/sdk.js' />
@@ -23,6 +24,6 @@ export default function FirstPost() {
 					<a>Back to home</a>
 				</Link>
 			</h2>
-		</>
+		</Layout>
 	);
 }
