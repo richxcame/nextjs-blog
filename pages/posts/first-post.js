266753323987acabe2a534b1f19d6a7c98f4f2aa
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-21 00:42:43 +0500

    L5: load 3rd scripts

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,22 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function FirstPost() {
 	return (
 		<>
 			<Head>
 				<title>First Post</title>
+				<script src='https://connect.facebook.net/en_US/sdk.js' />
 			</Head>
+			<Script
+				src='https://connect.facebook.net/en_US/sdk.js'
+				strategy='lazyOnload'
+				onLoad={() =>
+					console.log(`script loaded correctly, window.FB has been populated`)
+				}
+				onError={() => console.log('Error while fetching')}
+			/>
 			<h1>First Post</h1>
 			<h2>
 				<Link href='/'>
