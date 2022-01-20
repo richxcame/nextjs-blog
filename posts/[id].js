04fb25313a87f92ddd377b1c834e3c9f1c02d53d
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-21 01:51:37 +0500

    L4: create helper func

--- posts/[id].js
+++ posts/[id].js
@@ -1,3 +1,4 @@
+import { getAllPostIds } from '../../lib/posts';
 import Layout from '../../components/layout';
 
 export default function Post() {
@@ -6,6 +7,11 @@ export default function Post() {
 
 export async function getStaticPaths() {
 	// Return a list of possible value for id
+	const paths = getAllPostIds();
+	return {
+		paths,
+		fallback: false,
+	};
 }
 
 export async function getStaticProps({ params }) {
