5731d7d0406ad4de5360b0c237c1cc6f7dd7f7f0
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-21 13:32:02 +0500

    feat: add dynamic title

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Layout from '../../components/layout';
 
 export default function Post({ postData }) {
 	return (
 		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
 			{postData.title}
 			<br />
 			{postData.id}
